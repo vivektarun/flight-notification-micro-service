dd570e0079f475ca7f7cd7c78055cae416f80a53
vivektarun <vivektarun1234@gmail.com>
2025-08-11 23:52:17 +0530

    feat: Add ticket model and configure sample nodemailer service

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes'); // By default index.js in required.
 
+const mailSender = require('./config/email-config');
+const serverConfig = require('./config/server-config');
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
@@ -14,6 +17,18 @@ app.get('/', (req, res) => {
 
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
-    console.log(`Server running on http://localhost:${ServerConfig.PORT}`)
+app.listen(ServerConfig.PORT, async () => {
+    console.log(`Server running on http://localhost:${ServerConfig.PORT}`);
+    try {
+        const response = await mailSender.sendMail({
+            from: serverConfig.GMAIL_EMAIL,
+            to: 'vivektarun1234@gmail.com',
+            subject: 'Is the Service working now as well',
+            text: "yes it is working"
+        })
+    
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
 })
