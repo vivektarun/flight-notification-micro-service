49e9c4c7202e6013390f7d4ec284132afa3f302a
vivektarun <vivektarun1234@gmail.com>
2025-08-12 15:23:53 +0530

    feat: Add repository for ticket

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ const express = require('express');
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes'); // By default index.js in required.
 
-const mailSender = require('./config/email-config');
-const serverConfig = require('./config/server-config');
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
@@ -19,16 +16,4 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, async () => {
     console.log(`Server running on http://localhost:${ServerConfig.PORT}`);
-    try {
-        const response = await mailSender.sendMail({
-            from: serverConfig.GMAIL_EMAIL,
-            to: 'vivektarun1234@gmail.com',
-            subject: 'Is the Service working now as well',
-            text: "yes it is working"
-        })
-    
-        console.log(response);
-    } catch (error) {
-        console.log(error);
-    }
 })
