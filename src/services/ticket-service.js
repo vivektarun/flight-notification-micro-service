9c3b5b2a7e2acbb27491788ad546cfd825fcdec8
vivektarun <vivektarun1234@gmail.com>
2025-08-12 15:28:11 +0530

    feat: Add service to createTicket

--- src/services/ticket-service.js
+++ src/services/ticket-service.js
@@ -18,6 +18,17 @@ async function sendEmail(mailFrom, mailTo, subject, text) {
     }
 }
 
+async function createTicket(data) {
+    try {
+        const response = await ticketRepo.create(data);
+        return response;
+    } catch(error) {
+        console.log(error);
+        throw error;
+    }
+}
+
 module.exports = {
     sendEmail,
+    createTicket,
 }
